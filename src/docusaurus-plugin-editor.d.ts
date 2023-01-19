aa74b23ef2bffa84ce06d4e370ec803b85a21a8c
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-19 09:02:34 -0700

    Fix too long line for docusaurus-plugin-editor ambient module.

--- src/docusaurus-plugin-editor.d.ts
+++ src/docusaurus-plugin-editor.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="@docusaurus/plugin-content-docs" />
 
 declare module '@docusaurus/plugin-content-docs' {
-    import type { Options, PluginOptions } from '@docusaurus/plugin-content-docs';
+    import type {
+        Options,
+        PluginOptions
+    } from '@docusaurus/plugin-content-docs';
     import type { OptionValidationContext } from '@docusaurus/types';
 
     export function validateOptions(
